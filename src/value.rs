98ace1bc2533fccd794bb824e70ac15419f5a7cb
Jonny Arnold <jonny.arnold89@gmail.com>
2023-02-26 15:48:07 +0000

    WIP Numbers

--- src/value.rs
+++ src/value.rs
@@ -1,15 +1,18 @@
-use std::fmt::{Display, Formatter, Result};
+use std::{
+    fmt::{Display, Formatter, Result},
+    mem::size_of,
+};
 
 #[derive(Clone, Copy)]
 pub enum Value {
     Bool(bool),
-    Integer(i32), // TODO: General number
+    Number(i32),
 }
 
 const TYPE_BYTE_BOOL: u8 = 0x01;
-const TYPE_BYTE_INTEGER: u8 = 0x02;
+const TYPE_BYTE_NUMBER: u8 = 0x02;
 
-pub const VALUE_BYTES: usize = 8;
+pub const VALUE_BYTES: usize = size_of::<Value>();
 
 impl Value {
     // https://stackoverflow.com/a/74237171
@@ -25,10 +28,10 @@ impl Value {
                 0x00,
                 0x00,
             ],
-            Value::Integer(i) => {
-                // [TYPE_BYTE_INTEGER, ...i.to_be_bytes(), 0x00, 0x00, 0x00]
+            Value::Number(i) => {
+                // [TYPE_BYTE_NUMBER, ...i.to_be_bytes(), 0x00, 0x00, 0x00]
                 let mut v = Vec::with_capacity(8);
-                v.push(TYPE_BYTE_INTEGER);
+                v.push(TYPE_BYTE_NUMBER);
                 v.extend(i.to_be_bytes());
                 v.extend([0x00, 0x00, 0x00]);
                 return v.try_into().unwrap();
@@ -39,7 +42,7 @@ impl Value {
     pub fn from_bytes(bytes: &[u8]) -> Value {
         match bytes[0] {
             TYPE_BYTE_BOOL => Value::Bool(bytes[1] == 1),
-            TYPE_BYTE_INTEGER => Value::Integer(i32::from_be_bytes(bytes[1..5].try_into().unwrap())),
+            TYPE_BYTE_NUMBER => Value::Number(i32::from_be_bytes(bytes[1..5].try_into().unwrap())),
             _ => panic!("Unexpected Type Byte"),
         }
     }
@@ -49,7 +52,7 @@ impl Display for Value {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         match self {
             Value::Bool(b) => write!(f, "{}", b),
-            Value::Integer(i) => write!(f, "{}", i),
+            Value::Number(i) => write!(f, "{}", i),
         }
     }
 }
