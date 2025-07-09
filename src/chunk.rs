6bb07f19a7df075b902d5a209510293378916ba7
Jonny Arnold <jonny.arnold89@gmail.com>
2025-07-09 22:04:43 +0100

    End of Chapter 14

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,12 +1,15 @@
+use crate::value::Value;
 use std::fmt::{Display, Formatter};
 
 pub enum Op {
+    Constant(usize),
     Return,
 }
 
 impl Display for Op {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::Constant(i) => write!(f, "CONST {}", i),
             Self::Return => write!(f, "RETURN"),
         }
     }
@@ -14,25 +17,53 @@ impl Display for Op {
 
 pub struct Chunk {
     ops: Vec<Op>,
+    constants: Vec<Value>,
+    lines: Vec<usize>,
 }
 
 impl Chunk {
     pub fn new() -> Chunk {
-        Chunk { ops: vec![] }
+        Chunk {
+            ops: vec![],
+            constants: vec![],
+            lines: vec![],
+        }
     }
 
-    pub fn write(&mut self, op: Op) -> () {
+    pub fn add_op(&mut self, op: Op, line: usize) -> () {
         self.ops.push(op);
+        self.lines.push(line);
+    }
+
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        let new_constant_index = self.constants.len();
+        self.constants.push(value);
+        return new_constant_index;
     }
 }
 
 impl Display for Chunk {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let mut offset = 0;
+        let mut op_index = 0;
+
+        write!(f, "LN#  OP#  OP\n")?;
 
         for op in self.ops.iter() {
-            write!(f, "{:04}: {}\n", offset, op)?;
-            offset += 1;
+            if op_index > 0 && self.lines[op_index] == self.lines[op_index - 1] {
+                write!(f, "   | ")?;
+            } else {
+                write!(f, "{:04} ", self.lines[op_index])?;
+            }
+
+            write!(f, "{:04} {}", op_index, op)?;
+
+            match op {
+                Op::Constant(i) => write!(f, " [{}]", self.constants[*i])?,
+                _ => {}
+            };
+            write!(f, "\n")?;
+
+            op_index += 1;
         }
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod chunk;
+mod value;
 
 use chunk::{Chunk, Op};
 
 fn main() {
     let mut chunk = Chunk::new();
-    chunk.write(Op::Return.into());
+    let c1 = chunk.add_constant(1.2);
+    chunk.add_op(Op::Constant(c1), 1);
+    chunk.add_op(Op::Return.into(), 2);
     println!("{}", chunk);
 }
