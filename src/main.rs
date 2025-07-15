1b289030921ca247c7b4afdbecf162d772770938
Jonny Arnold <jonny.arnold89@gmail.com>
2025-07-15 22:24:54 +0100

    End of Chapter 15

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,19 @@ fn main() {
     let mut chunk = Chunk::new();
     let c1 = chunk.add_constant(1.2);
     chunk.add_op(Op::Constant(c1), 1);
-    chunk.add_op(Op::Return.into(), 2);
+
+    let c2 = chunk.add_constant(3.4);
+    chunk.add_op(Op::Constant(c2), 1);
+
+    chunk.add_op(Op::Add, 1);
+
+    let c3 = chunk.add_constant(5.6);
+    chunk.add_op(Op::Constant(c3), 1);
+
+    chunk.add_op(Op::Divide, 1);
+    chunk.add_op(Op::Negate, 1);
+    chunk.add_op(Op::Return, 1);
+
     println!("{}", chunk);
 
     let vm = VM::new(chunk);
