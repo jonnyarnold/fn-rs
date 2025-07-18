ead4a7410e3d2cb0eacff4a989824bbe3f817274
Jonny Arnold <jonny.arnold@nomio.com>
2025-07-18 13:57:17 +0100

    Added CLI; need to interpret() next!

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,49 @@ mod vm;
 use chunk::{Chunk, Op};
 use vm::VM;
 
-fn main() {
-    let mut chunk = Chunk::new();
-    let c1 = chunk.add_constant(1.2);
-    chunk.add_op(Op::Constant(c1), 1);
-    chunk.add_op(Op::Return.into(), 2);
-    println!("{}", chunk);
+use clap::{Parser, Subcommand};
+use std::io;
+use std::io::prelude::*;
 
-    let vm = VM::new(chunk);
-    vm.run();
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Start an interactive interpreter
+    Repl,
+
+    /// Execute a file
+    Run { path: String }
+}
+
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Repl => repl(),
+        Commands::Run { path } => {
+            println!("run! {}", path)
+        }
+    }
+}
+
+fn repl() {
+    // let mut vm = VM::new();
+
+    let mut line_buffer = String::new();
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Failed to flush output!");
+        io::stdin().read_line(&mut line_buffer).expect("Failed to get input!");
+        print!("{}", line_buffer);
+    }
 }
